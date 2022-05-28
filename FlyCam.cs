743b54fffda821256cfb39d1c554cd0e8bb296d8
Geoffroy Warin <code@geowarin.com>
2022-05-28 19:12:44 +0200

    wip

--- FlyCam.cs
+++ FlyCam.cs
@@ -1,21 +1,48 @@
 using Godot;
 
 public partial class FlyCam : CharacterBody3D {
-  [Export] public float Gravity = -24.8f;
   [Export] public float MaxSpeed = 50.0f;
   [Export] public float JumpSpeed = 18.0f;
   [Export] public float Accel = 10f;
   [Export] public float Deaccel = 16.0f;
-  [Export] public float MaxSlopeAngle = 40.0f;
   [Export] public float MouseSensitivity = 0.05f;
 
-  private Vector3 _vel;
-  private Vector3 _dir;
+  // private readonly float _gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
 
+#pragma warning disable CS8618
   private Camera3D _camera;
   private Node3D _rotationHelper;
+#pragma warning restore CS8618
 
-  // Called when the node enters the scene tree for the first time.
+  public override void _PhysicsProcess(float delta) {
+    var camXform = _camera.GlobalTransform;
+    var inputMovementVector = Input.GetVector("ui_left", "ui_right", "ui_down", "ui_up");
+    var dir = new Vector3();
+    dir += -camXform.basis.z * inputMovementVector.y;
+    dir += camXform.basis.x * inputMovementVector.x;
+    dir = dir.Normalized();
+
+    var vel = Velocity;
+
+    if (IsOnFloor() && Input.IsActionJustPressed("movement_jump")) {
+      vel.y = JumpSpeed;
+    }
+    // vel.y += delta * _gravity;
+
+    if (Input.IsActionJustPressed("ui_cancel")) {
+      Input.SetMouseMode(Input.GetMouseMode() == Input.MouseMode.Visible
+                             ? Input.MouseMode.Captured
+                             : Input.MouseMode.Visible);
+    }
+
+    var target = dir * MaxSpeed;
+    var accel = dir.Dot(vel) > 0 ? Accel : Deaccel;
+    vel = vel.Lerp(target, accel * delta);
+
+    Velocity = vel;
+    MoveAndSlide();
+  }
+
   public override void _Ready() {
     _camera = GetNode<Camera3D>("RotationHelper/Camera");
     _rotationHelper = GetNode<Node3D>("RotationHelper");
@@ -23,70 +50,12 @@ public partial class FlyCam : CharacterBody3D {
     Input.SetMouseMode(Input.MouseMode.Captured);
   }
 
-  public override void _PhysicsProcess(float delta) {
-    ProcessInput(delta);
-    ProcessMovement(delta);
-  }
-
-  private void ProcessInput(float delta) {
-    //  -------------------------------------------------------------------
-    //  Walking
-    _dir = new Vector3();
-    var camXform = _camera.GlobalTransform;
-
-    var inputMovementVector = Input.GetVector("ui_left", "ui_right", "ui_down", "ui_up");
-
-    // Basis vectors are already normalized.
-    _dir += -camXform.basis.z * inputMovementVector.y;
-    _dir += camXform.basis.x * inputMovementVector.x;
-    //  -------------------------------------------------------------------
-
-    //  -------------------------------------------------------------------
-    //  Jumping
-    if (IsOnFloor()) {
-      if (Input.IsActionJustPressed("movement_jump"))
-        _vel.y = JumpSpeed;
-    }
-    //  -------------------------------------------------------------------
-
-    //  -------------------------------------------------------------------
-    //  Capturing/Freeing the cursor
-    if (Input.IsActionJustPressed("ui_cancel")) {
-      Input.SetMouseMode(Input.GetMouseMode() == Input.MouseMode.Visible
-                             ? Input.MouseMode.Captured
-                             : Input.MouseMode.Visible);
-    }
-    //  -------------------------------------------------------------------
-  }
-
-  private void ProcessMovement(float delta) {
-    _dir.y = 0;
-    _dir = _dir.Normalized();
-
-    // _vel.y += delta * Gravity;
-
-    var hvel = _vel;
-    hvel.y = 0;
-
-    var target = _dir;
-
-    target *= MaxSpeed;
-
-    var accel = _dir.Dot(hvel) > 0 ? Accel : Deaccel;
-
-    hvel = hvel.Lerp(target, accel * delta);
-    _vel.x = hvel.x;
-    _vel.z = hvel.z;
-    Velocity = _vel;
-    MoveAndSlide();
-  }
-
   public override void _Input(InputEvent @event) {
     if (@event is InputEventMouseMotion mouseEvent &&
         Input.GetMouseMode() == Input.MouseMode.Captured) {
       _rotationHelper.RotateX(Mathf.Deg2Rad(mouseEvent.Relative.y * MouseSensitivity));
       RotateY(Mathf.Deg2Rad(-mouseEvent.Relative.x * MouseSensitivity));
-      
+
       // Vector3 cameraRot = _rotationHelper.RotationDegrees;
       // cameraRot.x = Mathf.Clamp(cameraRot.x, -70, 70);
       // _rotationHelper.RotationDegrees = cameraRot;
