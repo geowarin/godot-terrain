7ecc5841d276e3c20b3524ab4b1c6a17c21e329a
Geoffroy Warin <code@geowarin.com>
2022-06-05 19:47:35 +0200

    Generate heightmap shape

--- ColorTerrain.cs
+++ ColorTerrain.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using static Godot.Mathf;
 
 [Tool]
 public partial class ColorTerrain : Node3D {
@@ -31,10 +32,12 @@ public partial class ColorTerrain : Node3D {
 
   [Export] public float TerrainAccentuation = 31f;
   private MeshInstance3D? _meshInstance;
+  private CollisionShape3D? _collisionShape;
 
   private void Generate() {
     _meshInstance = GetNode<MeshInstance3D>("Mesh");
-    if (HeightMap is null || _meshInstance is null) {
+    _collisionShape = GetNode<CollisionShape3D>("StaticBody/CollisionShape");
+    if (HeightMap is null || _meshInstance is null || _collisionShape is null) {
       return;
     }
 
@@ -45,8 +48,29 @@ public partial class ColorTerrain : Node3D {
     surfaceMaterial.SetShaderParam("u_heightmap", heightmapTex);
     surfaceMaterial.SetShaderParam("u_tex", HeightMap);
 
+    _collisionShape.Shape = CreateHeightMapShape(heightmapTex);
+
     var image = HeightMap.GetImage();
     SizeMesh(image);
+  }
+
+  private HeightMapShape3D CreateHeightMapShape(Texture2D heightmapTex) {
+    var heightMapShape = new HeightMapShape3D();
+    var mapWidth = heightmapTex.GetWidth() + 1;
+    var mapHeight = heightmapTex.GetHeight() + 1;
+    heightMapShape.MapWidth = mapWidth;
+    heightMapShape.MapDepth = mapHeight;
+
+    var mapData = new float[mapWidth * mapHeight];
+
+    for (var index = 0; index < mapWidth * mapHeight; index++) {
+      var x = FloorToInt((float) index % mapWidth);
+      var y = FloorToInt((float) index / mapWidth);
+      var height = ImageUtils.GetPixelAverage(heightmapTex.GetImage(), x, y);
+      mapData[index] = height * TerrainAccentuation;
+    }
+    heightMapShape.MapData = mapData;
+    return heightMapShape;
   }
 
   private static Dictionary<Color, float> _levels = new() {
--- Terrain.cs
+++ Terrain.cs
@@ -63,32 +63,12 @@ public partial class Terrain : Node3D {
     for (var index = 0; index < mapWidth * mapHeight; index++) {
       var x = FloorToInt((float) index / mapWidth);
       var y = FloorToInt((float) index % mapWidth);
-      var height = GetPixelAverage(image, x, y);
+      var height = ImageUtils.GetPixelAverage(image, x, y);
       // GD.Print(x, " ", y, " => ", height);
       mapData[index] = height * TerrainAccentuation;
     }
 
     heightMapShape.MapData = mapData;
-  }
-
-  private static float GetPixelAverage(Image image, int x0, int y0) {
-    var h00 = GetPixelClamped(image, x0, y0);
-    var h10 = GetPixelClamped(image, x0 - 1, y0);
-    var h11 = GetPixelClamped(image, x0 - 1, y0 - 1);
-    var h01 = GetPixelClamped(image, x0, y0 - 1);
-
-    if (h00 == null && h10 == null && h01 == null && h11 == null) return 0f;
-
-    var sum = (h00?.r ?? 0f) + (h10?.r ?? 0f) + (h01?.r ?? 0f) + (h11?.r ?? 0f);
-    var num = (h00 == null ? 0f : 1f) + (h10 == null ? 0f : 1f) + (h01 == null ? 0f : 1f) +
-              (h11 == null ? 0f : 1f);
-    return sum / num;
-  }
-
-  private static Color? GetPixelClamped(Image im, int x, int y) {
-    if (x < 0 || x > im.GetWidth() - 1) return null;
-    if (y < 0 || y > im.GetHeight() - 1) return null;
-    return im.GetPixel(x, y);
   }
 
   public override void _Ready() {}
